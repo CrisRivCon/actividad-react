440b4db1cee5d6cf27f747bb24eec8344264638a
Cristina Rivera <cristinapruebas14@gmail.com>
2024-03-04 19:31:07 +0100

    Carrito en proceso 2

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -5,26 +5,28 @@ import getValueCookie from './GetCookies';
 
 function Boton(props) {
     const {texto, url, clases, tienda} = props;
+    const [carrito, setCarrito] = useState("");
 
 
     function anyadir() {
         if (tienda) {
             const articulo = props.tienda;
             const {Codigo} = articulo;
-            const [carrito, setCarrito] = useState("");
             let objetoCarrito = {};
 
             if (getValueCookie("carrito")) {
-                setCarrito(JSON.parse(getValueCookie("carrito")))
-                console.log(carrito);
+                objetoCarrito = JSON.parse(getValueCookie("carrito"));
+                console.log("objetoCarrito" , objetoCarrito);
+                objetoCarrito[Codigo] = objetoCarrito[Codigo] + 1;
+                setCarrito(JSON.stringify(objetoCarrito));
+
+                console.log(carrito, 'Existe carrito');
             } else {
                 objetoCarrito[Codigo] = 1;
                 setCarrito(JSON.stringify(objetoCarrito));
-                setCookie('carrito', carrito, 30);
-                console.log(carrito);
             }
             
-            setCookie(Codigo, Number(valor) + 1, 30);
+            setCookie("carrito", carrito, 30);
         }
     }
 
--- src/components/GetCookies.jsx
+++ src/components/GetCookies.jsx
@@ -1,6 +1,11 @@
+
 export default function getValueCookie(nombre) {
     const regex = new RegExp(`${nombre}=.*;?`, 'ig');
     let match = document.cookie.split(';').find((ele) => ele.match(regex));
+    if (!match){
+        return false;
+    }
     let valor = decodeURIComponent(match).split('=');
+    console.log(valor);
     return valor[1];
 }
